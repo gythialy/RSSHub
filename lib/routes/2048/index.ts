eda4ddb816967aa4a5b0f0f08e1bab0072ac8b5c
Goren G <gythialy.koo+github@gmail.com>
2024-10-12 12:55:35 +0800

    fix(route): kaiyan app

--- lib/routes/2048/index.ts
+++ lib/routes/2048/index.ts
@@ -67,10 +67,10 @@ async function handler(ctx) {
     const rootUrl = 'https://hjd2048.com';
     // 获取地址发布页指向的 URL
     const domainInfo = (await cache.tryGet('2048:domainInfo', async () => {
-        const response = await ofetch('https://2048.info');
+        const response = await ofetch('https://2048.bio');
         const $ = load(response);
-        const onclickValue = $('.button').first().attr('onclick');
-        const targetUrl = onclickValue.match(/window\.open\('([^']+)'/)[1];
+        const linkElement = $('div.address-list a:contains("论坛临时高速线路")');
+        const targetUrl = linkElement.attr('href');
 
         return { url: targetUrl };
     })) as { url: string };
